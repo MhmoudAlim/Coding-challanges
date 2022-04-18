7973df670cf1c66c404660c45c58c284e5c05788
MahmoudAlim <Mahmoudhusseinalim@gmail.com>
2022-04-18 16:22:56 +0200

    add ReverseString

---
+++ src/ReverseString.kt
@@ -0,0 +1,14 @@
+fun reverseString(): String {
+    val string = "ReverseMePlease"
+    var resultString = ""
+    var lastindex = string.lastIndex
+    while (lastindex >= 0) {
+        resultString = "$resultString${string[lastindex]}"
+        lastindex--
+    }
+    return resultString
+}
+
+fun main() {
+    println(reverseString())
+}
