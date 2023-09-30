0c6005de57fb9aef61d25a91caa9dc516c81d8a9
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2023-09-30 21:27:37 +0300

    update existing problems solutions

--- src/leetcode_problems/easy/ContainsDuplicate.kt
+++ src/leetcode_problems/easy/ContainsDuplicate.kt
@@ -1,9 +1,28 @@
 fun containsDuplicate(nums: IntArray): Boolean {
-    return nums.distinct().size < nums.size
+    // Naive solution
+//    return nums.distinct().size < nums.size
+
+    //solution 2
+//    nums.forEach{
+//        val newList = nums.toMutableList().also {list ->  list.remove(it) }
+//        if (newList.contains(it)) return true
+//    }
+//    return false
+
+
+    //solution 3
+    val hs = mutableSetOf<Int>()
+    nums.forEach {
+        if (hs.add(it).not()) return true
+    }
+    return false
+
 }
 
 
+
+
 fun main() {
-   val ans = containsDuplicate(nums = intArrayOf(1,2,3))
+   val ans = containsDuplicate(nums = intArrayOf(1,2,3,1))
     println(ans.toString())
 }
--- src/leetcode_problems/easy/TwoSum.kt
+++ src/leetcode_problems/easy/TwoSum.kt
@@ -1,17 +1,66 @@
 package leetcode_problems.easy
 
+import java.lang.Exception
+import java.util.HashMap
+import kotlin.math.abs
+
+
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    var ans = intArrayOf()
-    val map = mutableMapOf<Int, Int>()
-    for (i in nums.indices) {
-        if (map.contains(nums[i])) {
-            ans = map[nums[i]]?.let { intArrayOf(it, i) }!!
-            break
-        } else {
-            map[target - nums[i]] = i
-        }
-    }
-    return ans
+//    var ans = intArrayOf()
+//    val map = mutableMapOf<Int, Int>()
+//    for (i in nums.indices) {
+//        if (map.contains(nums[i])) {
+//            ans = map[nums[i]]?.let { intArrayOf(it, i) }!!
+//            break
+//        } else {
+//            map[target - nums[i]] = i
+//        }
+//    }
+//    return ans
+
+
+//    //solution 2
+//    nums.forEach {i ->
+//        val list = nums.toMutableList()
+//        list.remove(i)
+//        list.forEach {j ->
+//            if (i + j == target) return intArrayOf(nums.indexOf(i) ,nums.indexOf(j) )
+//        }
+//    }
+//    throw Exception("Not Found")
+
+
+
+//    var l = 0
+//    var r = nums.size - 1
+//    val list = nums.sorted()
+//    // [2,3,4]
+//    list.forEach {
+//        val middle = abs(r - l) / 2
+//        if (list[l] + list[middle] == target) return intArrayOf(l, middle)
+//        else if (list[l] + list[middle] > target)
+//            r--
+//        else{
+//            l = nums.size / 2
+//            r = nums.size -1
+//        }
+//    }
+//    throw Exception("Not found")
+//
+    //Solution 3
+//    val map = HashMap<Int, Int>()
+//    nums.forEachIndexed { i , num ->
+//        val diff = target - num
+//        if (map. containsKey(diff))
+//            return intArrayOf(map[diff]!! , i)
+//
+//        map[num] = i
+//
+//    }
+
+    return intArrayOf()
+
+
 }
 
 fun main() {
